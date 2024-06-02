84bb5831b97b4404dad9fdedc2605a9eb7587823
ilandaberea <ilandaberea@gmail.com>
2024-06-02 17:52:45 +0000

    añadido componente MenuItem + estilos

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
+import MenuItem from "./components/MenuItem";
 import { menuItems } from "./data/db"
 
 function App() {
-  console.log(menuItems);
-  
 
+  
   return (
     <>
       <header className='bg-teal-400 py-5'>
         <h1 className='text-center text-4xl font-black'>Calculadora de propinas y consumo</h1>
       </header>
+
+      <main className='mx-auto max-w-7xl py-20 grid md:grid-cols-2'>
+        <div>
+          <h2 className="text-4xl font-black">Menú</h2>
+          <div className='space-y-1 mt-10'>
+            {menuItems.map((item) => {
+                return <MenuItem key={item.id} item={item}/>
+              })
+
+            }
+
+          </div>
+        </div>
+        <div>
+          <h2>Consumo</h2>
+
+        </div>
+      </main>
     </>
   )
 }
---
+++ src/components/MenuItem.tsx
@@ -0,0 +1,16 @@
+import type { MenuItem } from "../types"
+type MenuItemProps = {
+    item: MenuItem
+}
+
+
+const MenuItem = ({item}: MenuItemProps) => {
+    return (
+        <button className='border-2 border-teal-400 w-full p-3 flex justify-between hover:bg-teal-100'>
+            <p>{item.name}</p>
+            <p className='font-black'>{item.price}€</p>
+        </button>)
+    
+}
+
+export default MenuItem
